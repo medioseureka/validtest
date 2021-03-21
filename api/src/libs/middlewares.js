d30189e96a41ee5c0a4e1217adba02c316475162
paobedoya <comercial@medioseureka.com>
2021-03-21 11:19:42 -0500

    updating process into database from frontend with alpine

--- api/src/libs/middlewares.js
+++ api/src/libs/middlewares.js
@@ -1,10 +1,12 @@
 import express from 'express';
+const cors = require('cors')
 
 module.exports = app => {
   // SETTINGS OF APP
   app.set('port', process.env.PORT || 3000);
 
   // middlewares
-  app.use(express.json())
+  app.use(express.json());
+  app.use(cors());
 };
 
